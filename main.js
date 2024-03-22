2cc89af2df6ec6232e009106fcca561ac6fc1534
Henoks247 <67102412+Henoks247@users.noreply.github.com>
2024-03-23 00:52:44 +0300

    test

--- main.js
+++ main.js
@@ -1,7 +1,1 @@
-<script>
-  var lottiePlayer = document.getElementsByTagName("lottie-player"); var
-  hoverTrigger = document.getElementById("lottie-controller");
-  lottie-controller.addEventListener('mouseover', function(event){" "}
-  {lottiePlayer[0].pause()}); lottie-controller.addEventListener('mouseout',
-  function(event) {lottiePlayer[0].play()});
-</script>;
+console.log("code link functionality check");

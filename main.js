1c45468914ec3b8653f9a4d537a11992001b65a5
Henoks247 <67102412+Henoks247@users.noreply.github.com>
2024-03-23 02:03:52 +0300

    background

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 console.log("code link functionality check");
+document.getElementById("lottie-controller").style.backgroundColor =
+  "lightblue";
+
+// var lottiePlayer = document.getElementsByTagName("lottie-player");
+// var hoverTrigger = document.getElementById("lottie-controller");
+
+// lottie-controller.addEventListener('mouseover', function(event) {
+//   lottiePlayer[0].pause()
+// });
+
+// lottie-controller.addEventListener('mouseout', function(event) {
+//   lottiePlayer[0].play()
+// });

069ab668d04f72a23e534856ae1bcdd0ca9a1653
Henoks247 <67102412+Henoks247@users.noreply.github.com>
2024-03-23 11:38:24 +0300

    hover

--- main.js
+++ main.js
@@ -3,12 +3,6 @@ console.log("code link functionality check 2");
 let animation = document.getElementById("exploding-bird");
 console.log(animation);
 
-getElementById("lottiecontroller").addEventListener("mouseover", trialfunction);
-
-function trialfunction() {
-  document.getElementById("lottiecontroller").style.backgroundColor = "red";
-}
-
 var lottiePlayer = document.getElementsByTagName("lottie-player");
 var hoverTrigger = document.getElementById("lottiecontroller");
 

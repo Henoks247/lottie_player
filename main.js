2528b47102de52794e3455d1f5f7867eb6c85bbc
Henoks247 <67102412+Henoks247@users.noreply.github.com>
2024-03-23 11:45:11 +0300

    logged

--- main.js
+++ main.js
@@ -4,12 +4,15 @@ let animation = document.getElementById("exploding-bird");
 console.log(animation);
 
 var lottiePlayer = document.getElementsByTagName("lottie-player");
+console.log(2);
 var hoverTrigger = document.getElementById("lottiecontroller");
-
+console.log(3);
 lottiecontroller.addEventListener("mouseover", function (event) {
   lottiePlayer.pause();
+  console.log(4);
 });
 
 lottiecontroller.addEventListener("mouseout", function (event) {
   lottiePlayer.play();
+  console.log(5);
 });
